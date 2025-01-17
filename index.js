46033124c383c792dfb468b0d08c7c7ec1c118f3
tejasKhamkar2211 <tejaskhamkar23@gmail.com>
2025-01-17 13:22:21 +0530

    7th commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Good afternoon');
+ res.send('Have you taken your lounch');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
