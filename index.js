9918a9edcce53b7352627b7a7ea83247653d425f
tejasKhamkar2211 <tejaskhamkar23@gmail.com>
2025-01-20 13:13:02 +0530

    7th commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Focus on one task at a time for better learning.');
+ res.send('Create a Jenkins pipeline to pull the latest Docker image and deploy it to a new node for continuous integration.');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
