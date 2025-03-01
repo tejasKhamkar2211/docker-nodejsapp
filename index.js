55c375b261f5a1a1733d96f50c271730fb66dba4
tejasKhamkar2211 <tejaskhamkar23@gmail.com>
2025-03-01 16:28:10 +0530

    tejas

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
  
 var app = express();//Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Dnyaneshwar  mauli guru dnyanraj mauli tukara ');
+ res.send('ram krishna hari ');
 });//listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
